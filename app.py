d02c1350ddc0453b43c2ab690ebdedabd104c196
choirulanwar <38089394+choirulanwar@users.noreply.github.com>
2023-12-10 12:25:27 +0700

    basic semantic keyword extraction

--- app.py
+++ app.py
@@ -1,35 +1,81 @@
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
-
 from pydantic import BaseModel
+from sklearn.feature_extraction.text import TfidfVectorizer
+import spacy
+from keyword_spacy import KeywordExtractor
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import Any
+import numpy as np
+
+
+class TextModel(BaseModel):
+    text: str
+
+
+def numpy_encoder(obj: Any):
+    if isinstance(obj, np.float32):
+        return float(obj)
+    raise TypeError
+
 
 app = FastAPI()
 
+try:
+    nlp = spacy.load("en_core_web_md")
+except OSError:
+    spacy.cli.download("en_core_web_md")
+    nlp = spacy.load("en_core_web_md")
 
-class Item(BaseModel):
-    item_id: int
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse('favicon.ico')
+nlp.add_pipe("keyword_extractor", last=True, config={"top_n": 100,
+                                                     "top_n_sent": 100,
+                                                     "min_ngram": 2,
+                                                     "max_ngram": 3,
+                                                     "strict": True})
 
 
-@app.get("/item/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+@app.post("/extract-keywords/")
+async def extract_keywords(text_model: TextModel):
+    doc = nlp(text_model.text)
 
+    keyword_data = [
+        {
+            "term": term,
+            "frequency": frequency,
+            "score": score
+        }
+        for term, frequency, score in doc._.keywords
+    ]
+    json_compatible_item_data = jsonable_encoder(
+        keyword_data, custom_encoder={np.float32: numpy_encoder})
 
-@app.get("/items/")
-async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
+    return JSONResponse(content=json_compatible_item_data)
+    # # Proses teks dengan Spacy untuk mendapatkan noun phrases
+    # doc = nlp(text_model.text)
+    # noun_phrases = [chunk.text.lower().strip()
+    #                 for chunk in doc.noun_chunks if chunk.text.strip()]
 
+    # # Menghitung TF-IDF skor untuk setiap noun phrase
+    # vectorizer = TfidfVectorizer()
+    # vectors = vectorizer.fit_transform(noun_phrases)
+    # feature_names = vectorizer.get_feature_names_out()
+    # scores = vectors.toarray().flatten()
 
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+    # # Membuat mapping antara noun phrases dan skor TF-IDF mereka
+    # keywords_score = list(zip(noun_phrases, scores))
+
+    # # Mengurutkan berdasarkan skor tertinggi
+    # sorted_keywords = sorted(
+    #     keywords_score, key=lambda tup: tup[1], reverse=True)
+
+    # # Mengkonversi ke format yang diinginkan: array of dict dengan "term", "frequency", dan "score"
+    # keyword_data = [
+    #     {
+    #         "term": keyword,
+    #         "score": score,
+    #     }
+    #     for keyword, score in sorted_keywords
+    # ]
+
+    return keyword_data
